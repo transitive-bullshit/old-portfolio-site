7b04cf8ec3d2e80038a6e39e2728038f5a182607
Travis Fischer <fisch0920@gmail.com>
2018-12-17 07:54:05 -0500

    .

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import './global.css'
+
 import App from './components/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
